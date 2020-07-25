71754e57ae75a0a2168f6a12984644636a6e4e3a
Kamal Lama <kamal09rumba@gmail.com>
2020-07-25 21:17:41 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// import a library to help to create a component
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
+import Header from './src/components/header';
+
+// create a component
+
+const App = () => <Header />;
+
+// render it to the device
+AppRegistry.registerComponent('albums', () => App);
